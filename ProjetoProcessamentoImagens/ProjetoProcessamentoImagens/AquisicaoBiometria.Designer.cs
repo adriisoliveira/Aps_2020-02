422a8e72cdbac96c66d25a39c9fce9f0f5d7a20a
Johny Firmino <johnyfirmino@hotmail.com>
2020-10-19 19:34:23 -0300

    teste

--- ProjetoProcessamentoImagens/ProjetoProcessamentoImagens/AquisicaoBiometria.Designer.cs
+++ ProjetoProcessamentoImagens/ProjetoProcessamentoImagens/AquisicaoBiometria.Designer.cs
@@ -31,6 +31,7 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnUP = new System.Windows.Forms.Button();
             this.pbImagem = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbImagem)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,9 +41,10 @@
             // 
             // btnUP
             // 
-            this.btnUP.Location = new System.Drawing.Point(643, 284);
+            this.btnUP.Location = new System.Drawing.Point(482, 231);
+            this.btnUP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnUP.Name = "btnUP";
-            this.btnUP.Size = new System.Drawing.Size(100, 50);
+            this.btnUP.Size = new System.Drawing.Size(75, 41);
             this.btnUP.TabIndex = 3;
             this.btnUP.Text = "Subir";
             this.btnUP.UseVisualStyleBackColor = true;
@@ -51,19 +53,32 @@
             // pbImagem
             // 
             this.pbImagem.ErrorImage = null;
-            this.pbImagem.Location = new System.Drawing.Point(57, 50);
+            this.pbImagem.Location = new System.Drawing.Point(43, 41);
+            this.pbImagem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbImagem.Name = "pbImagem";
-            this.pbImagem.Size = new System.Drawing.Size(500, 350);
+            this.pbImagem.Size = new System.Drawing.Size(375, 284);
             this.pbImagem.TabIndex = 2;
             this.pbImagem.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 93);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Subir";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // AquisicaoBiometria
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnUP);
             this.Controls.Add(this.pbImagem);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AquisicaoBiometria";
             this.Text = "AquisicaoBiometria";
             ((System.ComponentModel.ISupportInitialize)(this.pbImagem)).EndInit();
@@ -76,5 +91,6 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnUP;
         private System.Windows.Forms.PictureBox pbImagem;
+        private System.Windows.Forms.Button button1;
     }
 }
