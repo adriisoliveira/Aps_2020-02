d7d51801c992a0cb0bdaf17765d8fdcfa246a6cf
Adrianne Sousa <65831740+adriisoliveira@users.noreply.github.com>
2020-10-19 23:01:35 -0300

    Arquivo de inserções do banco de dados

---
+++ Inserções no Banco de Dados.sql
@@ -0,0 +1,72 @@
+
+use Ministerio_MeioAmbiente
+
+/*Inserções*/
+
+INSERT INTO Usuario VALUES ('010101','Adrianne Sousa','11-982828282','3','Adrianne','123456')
+
+
+select * from Usuario
+
+INSERT INTO Estado VALUES 
+('Acre', 'AC'),
+('Alagoas', 'AL'),
+('Amapá', 'AP'),
+('Amazonas','AM'),
+('Bahia', 'BA'),
+('Ceará', 'CE'),
+('Distrito federal','DF'),
+('Espirito santo', 'ES'),
+('Goiás','GO'),
+('Maranhão', 'MA'),
+('Mato Grosso','MT'),
+('Mato Grosso do Sul','MS'),
+('Minas Gerais', 'MG'),
+('Pará', 'PA'),
+('Paraíba', 'PB'),
+('Paraná', 'PR'),
+('Pernambuco','PE'),
+('Piauí', 'PI'),
+('Rio de janeiro','RJ'),
+('Rio Grande do Norte', 'RN'),
+('Rio Grande do Sul', 'RS'),
+('Rondônia','RO'),
+('Roraima', 'RR'),
+('Santa Catarina', 'SC'),
+('São Paulo','SP'),
+('Sergipe','SE'),
+('Tocantins','TO');
+
+
+select * from Estado
+
+INSERT INTO Cidade VALUES 
+('Rio Branco',1),
+('Maceió',2),
+('Macapá',3),
+('Manaus',4),
+('Salvador',5),
+('Fortaleza',6),
+('Brasília',7),
+('Vitória',8),
+('Goiânia',9),
+('São Luís',10),
+('Cuiabá',11),
+('Campo Grande',12),
+('Belo Horizonte',13),
+('Belém',14),
+('João Pessoa',15),
+('Curitiba',16),
+('Recife',17),
+('Teresina',18),
+('Rio de Janeiro',19),
+('Natal',20),
+('Porto Alegre',21),
+('Porto Velho',22),
+('Boa Vista',23),
+('Florianópolis',24),
+('São Paulo',25),
+('Aracaju',26),
+('Palmas',27);
+
+select * from Cidade
